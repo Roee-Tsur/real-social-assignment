68aa54e591f658450005ba73777a737a851c1b05
Surk217 <tsur.roee@gmail.com>
2023-05-11 09:36:06 +0300

    initial camera position

--- lib/screens/home/home_view_impl.dart
+++ lib/screens/home/home_view_impl.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -14,10 +15,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final double defaultZoom = 10.5;
+
   Location location = Location();
+  LocationData? currentLocation;
+
   bool serviceEnabled = false;
-  User? user;
   bool locationPermissionGranted = false;
+  bool isStyleLoaded = false;
+  MapboxMapController? mapController;
+  bool isCameraPositionInitialized = false;
+
+  User? user;
 
   @override
   void initState() {
@@ -27,20 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
         this.user = user;
       }),
     );
-    Permission.locationWhenInUse.request().then((value) {
-      setState(() {
-        locationPermissionGranted = value.isGranted;
-      });
-      location.serviceEnabled().then((isEnabled) {
-        serviceEnabled = isEnabled;
-        if (!isEnabled) {
-          location.requestService().then((isActive) {
-            serviceEnabled = isActive;
-          });
-        }
-      });
-    });
-
+    initLocation();
     super.initState();
   }
 
@@ -56,8 +52,62 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: MapboxMap(
-          accessToken: "pk.eyJ1Ijoicm9lZXRzdXIiLCJhIjoiY2xoaTV1Y241MDRtbDNmcGhpODR4NDloOSJ9.wL3-GnuJ63LDA9-8DYX4Ew",
-          initialCameraPosition: CameraPosition(target: LatLng(10, 10))),
+        accessToken:
+            "pk.eyJ1Ijoicm9lZXRzdXIiLCJhIjoiY2xoaTV1Y241MDRtbDNmcGhpODR4NDloOSJ9.wL3-GnuJ63LDA9-8DYX4Ew",
+        //default initial camera position is tel aviv
+        initialCameraPosition: CameraPosition(
+            target: const LatLng(32.075982, 34.787155), zoom: defaultZoom - 1),
+        myLocationEnabled: true,
+        onStyleLoadedCallback: () {
+          setState(() {
+            isStyleLoaded = true;
+          });
+        },
+        onMapCreated: (controller) {
+          mapController = controller;
+          if (currentLocation != null && !isCameraPositionInitialized) {
+            if (currentLocation!.latitude != null &&
+                currentLocation!.longitude != null) {
+              controller.animateCamera(CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      zoom: defaultZoom,
+                      target: LatLng(currentLocation!.latitude!,
+                          currentLocation!.longitude!))));
+            }
+          }
+        },
+      ),
     );
+  }
+
+  Future<void> initLocation() async {
+    locationPermissionGranted =
+        await Permission.locationWhenInUse.request().isGranted;
+    if (!locationPermissionGranted) {
+      Fluttertoast.showToast(msg: 'No location permission');
+      return;
+    }
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        Fluttertoast.showToast(msg: "No location service");
+        return;
+      }
+    }
+    location.getLocation().then((value) {
+      if (value.latitude != null && value.longitude != null) {
+        if (mapController != null) {
+          mapController!.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  zoom: defaultZoom,
+                  target: LatLng(value.latitude!, value.longitude!))));
+        }
+        setState(() {
+          currentLocation = value;
+        });
+      }
+    });
   }
 }
